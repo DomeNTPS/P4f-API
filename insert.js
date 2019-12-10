8424346c28033eedd0ba34bbb6d9a6f8302dc298
Chalermwut <chalermwut13@hotmail.com>
2019-12-10 21:49:28 +0700

    1 job

--- insert.js
+++ insert.js
@@ -3,16 +3,16 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database:"p4f"
+    database: "p4f"
 
 });
 
-con.connect((err)=>{
-    if(err) throw err;
+con.connect((err) => {
+    if (err) throw err;
     console.log("Connected!");
     sql = `INSERT INTO running_equipment(IDpath, KKScode, DateStart,DateExpired) VALUES (UUID(),'10LAB10AA002','2019-12-10','2020-12-10')`
-    con.query(sql,function(err,result){
-        if(err) throw err;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
         console.log("Insert Complete!");
 
         
---
+++ select.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "p4f"
+
+});
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+    sql = "select * from running_equipment";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
