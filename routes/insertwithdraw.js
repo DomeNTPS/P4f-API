c1bb3442a5df79f64e499727c70e26d188c92f0b
Chalermwut <chalermwut13@hotmail.com>
2020-02-11 15:56:34 +0700

    update

--- routes/insertwithdraw.js
+++ routes/insertwithdraw.js
@@ -12,7 +12,7 @@ router.post('/', function (req, res, next) {
     //  console.log(req.body.KKS4_Equip_Withdraw)
     //  console.log(req.body.Count_withdraw)
     //  console.log(req.body.Date_Withdraw)
-    //  var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,Count_withdraw,Date_Withdraw) VALUES('1379900073717', 'AA',' 1','2020-02-07')`;
+     var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,Count_withdraw,Date_Withdraw) VALUES('1379900073717', 'AA',' 1','2020-02-07')`;
 
      var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,Count_withdraw,Date_Withdraw) VALUES('${req.body.IDEmp}', '${req.body.KKS4_Equip_Withdraw}', '${req.body.Count_withdraw}','${req.body.Date_Withdraw}')`;
     db.query(sql, function (err, rows, fields) {
