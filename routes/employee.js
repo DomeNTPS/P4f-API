081d1b60258be720693d199f06d1b07217dece38
Nontakon <59010679@kmitl.ac.th>
2020-02-12 22:11:31 +0700

    post login

--- routes/employee.js
+++ routes/employee.js
@@ -1,27 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../db');
-var bodyParser = require('body-parser');
-
+var db = require("../db");
+var bodyParser = require("body-parser");
+import jwt from "react-native-pure-jwt";
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/:id', function (req, res, next) {
+router.post("/", function(req, res, next) {
+  console.log(req.body);
+  //  console.log("-------------------------------------------------------")
 
-    const {
-        id
-    } = req.params
-    var password = 123456789
-    console.log(id, req.params)
-    var sql = `SELECT Position,KKS1_factory FROM employee WHERE IDEmp="${id}" AND Password="${password}"`;
-    db.query(sql, function (err, rows, fields) {
-        if (err) {
-            res.status(500).send({
-                error: 'Something failed!'
-            })
-        }
-        console.log(rows, fields)
-         res.json(rows[0])
-    })
+  //  console.log(req.body)
+  console.log(req.body.ID);
+  console.log(req.body.Pass);
+  //  console.log(req.body.Date_Withdraw)
+  //  var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,Count_withdraw,Date_Withdraw) VALUES('1379900073717', 'AA',' 1','2020-02-07')`;
+
+  var sql = `SELECT IDEmp,Position,KKS1_factory FROM employee WHERE IDEmp="${req.body.ID}" AND Password="${req.body.Pass}"`;
+  db.query(sql, function(err, rows, fields) {
+    if (err) {
+      res.status(500).send({
+        err: console.error()
+      });
+    }
+    // console.log(rows, fields)
+    console.log(rows[0].IDEmp);
+    res.send(rows[0]);
+  });
 });
 
 module.exports = router;
