1501629f3c1dce66a8252386e52783dc40f08b8f
Nontakon <59010679@kmitl.ac.th>
2020-02-23 15:52:47 +0700

    fix bug

--- routes/insertwithdraw.js
+++ routes/insertwithdraw.js
@@ -29,7 +29,7 @@ router.post('/', function (req, res, next) {
     //  console.log(req.body.Date_Withdraw)
     //  var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,Count_withdraw,Date_Withdraw) VALUES('1379900073717', 'AA',' 1','2020-02-07')`;
 
-     var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,KKS1_factory_withdraw,Count_withdraw,Date_Withdraw) VALUES('${IDEmp.ID}', '${req.body.KKS4_Equip_Withdraw}',''${req.body.KKS1}, '${req.body.Count_withdraw}','${req.body.Date_Withdraw}')`;
+     var sql = `INSERT INTO withdraw(IDEmp,KKS4_Equip_Withdraw,KKS1_Factory_withdraw,Count_withdraw,Date_Withdraw) VALUES('${IDEmp.ID}', '${req.body.KKS4_Equip_Withdraw}','${req.body.KKS1}', '${req.body.Count_withdraw}','${req.body.Date_Withdraw}')`;
     db.query(sql, function (err, rows, fields) {
         if (err) {
             res.status(500).send({
