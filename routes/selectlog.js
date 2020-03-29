2c5ac1eba0315c159480d574142db19ed89eede9
Nontakon <59010679@kmitl.ac.th>
2020-03-30 03:30:36 +0700

    selectlog with KKS1

--- routes/selectlog.js
+++ routes/selectlog.js
@@ -26,5 +26,21 @@ router.get('/:datetime', function (req, res, next) {
         res.json(rows)
     })
 });
-
+router.post('/', function (req, res, next) {
+    console.log(req.body)
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,Process,DateLog,CountLog
+    FROM employee,equipment,loginventory
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01" AND "${req.body.Year}-${req.body.Month}-31" AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
+    // var sql = `SELECT *
+    // FROM withdraw
+    // WHERE Date_withdraw = "${datetime}-01"`;
+    db.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.send(rows)
+        }
+        console.log(rows)
+        res.json(rows)
+    })
+});
 module.exports = router;
