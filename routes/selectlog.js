0a898596a4264af1f3b975e0ae5c29a27064fccb
DomeNTPS <tanndome_555@hotmail.com>
2020-04-11 20:55:35 +0700

    selectlog code update
    Co-authored-by: Nontakon Charoencheewakul <59010679@kmitl.ac.th>
    Co-authored-by: Sorayuth C <sorayuth10@gmail.com>
    Co-authored-by: Jakkapat Boonroj <max.jakkapat@gmail.com>

--- routes/selectlog.js
+++ routes/selectlog.js
@@ -5,15 +5,14 @@ var bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/:datetime', function (req, res, next) {
-
-    const {
-        datetime
-    } = req.params
-    console.log(req.params)
-    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,Process,DateLog,CountLog
+router.post('/withdraw', function (req, res, next) {
+    console.log(req.body)
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,DateLog,CountLog
     FROM employee,equipment,loginventory
-    WHERE DateLog BETWEEN "${datetime}-01" AND "${datetime}-31"
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01"
+    AND "${req.body.Year}-${req.body.Month}-31"
+    AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND loginventory.Process = "WithdrawEquipment"
     AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
     // var sql = `SELECT *
     // FROM withdraw
@@ -26,11 +25,14 @@ router.get('/:datetime', function (req, res, next) {
         res.json(rows)
     })
 });
-router.post('/', function (req, res, next) {
+router.post('/add', function (req, res, next) {
     console.log(req.body)
-    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,Process,DateLog,CountLog
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,DateLog,CountLog
     FROM employee,equipment,loginventory
-    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01" AND "${req.body.Year}-${req.body.Month}-31" AND loginventory.KKS1 = "${req.body.KKS1}"
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01"
+    AND "${req.body.Year}-${req.body.Month}-31"
+    AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND loginventory.Process = "AddEquipment"
     AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
     // var sql = `SELECT *
     // FROM withdraw
@@ -39,7 +41,67 @@ router.post('/', function (req, res, next) {
         if (err) {
             res.send(rows)
         }
-        console.log(rows)
+        // console.log(rows)
+        res.json(rows)
+    })
+});
+router.post('/changeequipment', function (req, res, next) {
+    console.log(req.body)
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,loginventory.KKSCode,DateLog
+    FROM employee,equipment,loginventory
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01"
+    AND "${req.body.Year}-${req.body.Month}-31"
+    AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND loginventory.Process = "ChangeEquipment"
+    AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
+    // var sql = `SELECT *
+    // FROM withdraw
+    // WHERE Date_withdraw = "${datetime}-01"`;
+    db.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.send(rows)
+        }
+        // console.log(rows)
+        res.json(rows)
+    })
+});
+router.post('/return', function (req, res, next) {
+    console.log(req.body)
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,DateLog,CountLog
+    FROM employee,equipment,loginventory
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01"
+    AND "${req.body.Year}-${req.body.Month}-31"
+    AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND loginventory.Process = "ReturnEquipment"
+    AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
+    // var sql = `SELECT *
+    // FROM withdraw
+    // WHERE Date_withdraw = "${datetime}-01"`;
+    db.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.send(rows)
+        }
+        // console.log(rows)
+        res.json(rows)
+    })
+});
+router.post('/broke', function (req, res, next) {
+    console.log(req.body)
+    var sql = `SELECT NameEmp,LastNameEmp,employee.IDEmp,NameEquip,DateLog,CountLog
+    FROM employee,equipment,loginventory
+    WHERE DateLog BETWEEN "${req.body.Year}-${req.body.Month}-01"
+    AND "${req.body.Year}-${req.body.Month}-31"
+    AND loginventory.KKS1 = "${req.body.KKS1}"
+    AND loginventory.Process = "EquipmentBroke"
+    AND employee.IDEmp = loginventory.IDEmp AND loginventory.KKS4 = equipment.KKS4`;
+    // var sql = `SELECT *
+    // FROM withdraw
+    // WHERE Date_withdraw = "${datetime}-01"`;
+    db.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.send(rows)
+        }
+        // console.log(rows)
         res.json(rows)
     })
 });
