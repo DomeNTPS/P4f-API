29ece82e56c530d4344129eb3ba22ebf67382a86
DomeNTPS <tanndome_555@hotmail.com>
2020-05-02 20:39:46 +0700

    chang to new server
    Co-authored-by: Nontakon Charoencheewakul <59010679@kmitl.ac.th>

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: '34.87.55.206',
+    host: '34.87.165.18',
     port: 3306,
     user: 'root',
     password: 'Chalermwut13',
@@ -12,7 +12,5 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log('connected!');
 });
-
-
 
 module.exports = connection;
